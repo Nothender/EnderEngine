37c23c6fe23303b6211cc1902f79d9f917594ce2
Someone-42 <Someone-42@Nothender.noreply.github.com>
2020-11-15 18:30:28 +0100

    Name Change

---
+++ EnderEngineExampleApp-CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EnderEngine;
+
+namespace EnderEngineExampleApp
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Engine.Test();
+        }
+    }
+}
