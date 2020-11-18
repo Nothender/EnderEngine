87791e009ed8fe86736babe0fe6fa04b51c1c40c
Someone-42 <Someone-42@Nothender.noreply.github.com>
2020-11-18 23:29:34 +0100

    Moved Logger + Fixed methods, logging working

---
+++ EnderEngine/Core/Logger.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace EnderEngine
+{
+    public class Logger
+    {
+        /// <summary>
+        /// Name of the log file (static, cause every logger will log into the same file - the prefix and message will differenciate them)
+        /// </summary>
+        private static readonly string LogsFileName = $"logs {DateTime.Now}".Replace(' ', '_').Replace('/', '-').Replace(':', '-') + ".log"; //TODO: Change extension handling with proper FS
+        /// <summary>
+        /// Path to the log file
+        /// </summary>
+        private string LogsFilePath;
+        /// <summary>
+        /// If this is true, when logging the date and time will be added in the prefix, {}
+        /// </summary>
+        public static bool DoWriteDateAndTime = true;
+
+        public Logger()
+        {
+            LogsFilePath = "mmm/" + LogsFileName;
+            //TODO: Check for file created (use File System later, to make that check simpler, use event system to do it only once at startup)
+            if (System.IO.File.Exists(LogsFilePath))
+                return;
+            System.IO.File.Create(LogsFilePath).Close();
+        }
+
+        public static void EnableDateAndTimeWriting(bool enable)
+        {
+            DoWriteDateAndTime = enable;
+        }
+        internal string GetLogPrefix(LogLevel logLevel)
+        {
+            return "[EnderEngine] " + (DoWriteDateAndTime ? "{" + DateTime.Now + "} " : "") + $"[{logLevelsStringArray[(int)logLevel]}]";
+        }
+
+        /// <summary>
+        ///  Enables to log a given string into a file/console. By default, the method will log into a file and into the console
+        /// </summary>
+        public void Log(string message, LogLevel logLevel, LogMethod method = LogMethod.TO_FILE_AND_CONSOLE)
+        {
+            string prefix = GetLogPrefix(logLevel);
+            if ((int)method < 2) //If we want to log in the Console (LogMethod.TO_CONSOLE or TO_CONSOLE_AND_FILE)
+                Console.WriteLine($"{prefix}: {message}");
+            if (method > 0) //If we want to log into the log file (LogMethod.TO_CONSOLE_AND_FILE or TO_FILE)
+            {
+                using (System.IO.StreamWriter text = System.IO.File.AppendText(this.LogsFilePath))
+                {
+                    text.WriteLine($"{prefix}: {message}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// The log level to be defined, how critical the log is
+        /// </summary>
+        public enum LogLevel
+        {
+            FATAL = 0,
+            ERROR,
+            WARN,
+            DEBUG,
+            INFO
+        }
+
+        /// <summary>
+        /// stores the LogLevel strings and can be picked from the LogLevel int index
+        /// </summary>
+        private readonly string[] logLevelsStringArray =
+        {
+            "FATAL",
+            "Error",
+            "Warn",
+            "Debug",
+            "Info"
+        };
+
+        /// <summary>
+        /// The log method is how the message will be logged
+        /// </summary>
+        public enum LogMethod
+        {
+            TO_CONSOLE = 0,
+            TO_FILE_AND_CONSOLE = 1,
+            TO_FILE = 2
+        }
+    }
+}
--- EnderEngineExampleApp-CSharp/Program.cs
+++ EnderEngineExampleApp-CSharp/Program.cs
@@ -7,7 +7,18 @@ namespace EnderEngineExampleApp
     {
         private static void Main(string[] args)
         {
-            Engine.Test();
+            Logger logg = new Logger();
+            for (int i = 0; i < 42000; i++)
+            {
+                logg.Log("42", Logger.LogLevel.FATAL);
+                logg.Log("42", Logger.LogLevel.WARN);
+                logg.Log("42", Logger.LogLevel.INFO);
+                Logger.DoWriteDateAndTime = false;
+                
+                logg.Log("42", Logger.LogLevel.DEBUG);
+                logg.Log("42", Logger.LogLevel.ERROR);
+                logg.Log("42", Logger.LogLevel.FATAL);
+            }
         }
     }
 }
