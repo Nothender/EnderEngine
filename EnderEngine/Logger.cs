0ad0fd005a093efcd14b573560614caabc7207e9
Mortell560 <mortell560@gmail.com>
2020-11-18 11:45:05 +0100

    Fixed Naming Conventions

--- EnderEngine/Logger.cs
+++ EnderEngine/Logger.cs
@@ -4,86 +4,91 @@ namespace EnderEngine
 {
     public abstract class LogBase
     {
-        public abstract void Log(string Message, Logger.LogLevel Flag, Logger.LogMethod method = Logger.LogMethod.ToFile);
+        public abstract void Log(string Message, Logger.LogLevel Flag, Logger.LogMethod method = Logger.LogMethod.TO_FILE);
     }
 
     public class Logger : LogBase
     {
         public enum LogLevel
         {
-            Fatal,
-            Error,
-            Warn,
-            Debug,
-            Info
+            FATAL,
+            ERROR,
+            WARN,
+            DEBUG,
+            INFO
         }
 
         public enum LogMethod
         {
-            ToConsole,
-            ToFile,
-            ToFileAndConsole
+            TO_CONSOLE,
+            TO_FILE,
+            TO_FILE_AND_CONSOLE
         }
-
-        private string LogName { get; set; } // get and set the Name of the log file 
-        private string LogPath { get; set; } // get and set the Path of the log file
+        /// <summary>
+        /// Name of the log file 
+        /// </summary>
+        private string LogFileName { get; set; }
+        /// <summary>
+        /// Path of the log file
+        /// </summary>
+        private string LogFilePath { get; set; }
 
 
         public Logger()
         {
-            this.LogName = "Log.txt";
-            this.LogPath = "Logs/" + this.LogName;
+            this.LogFileName = "Log.txt";
+            this.LogFilePath = "Logs/" + this.LogFileName;
         }
 
         /// <summary>
         ///  Enables to log a given string into a file/console. By default, the method will log into a file and into the console
         /// </summary>
-        public override void Log(string Message, LogLevel Flag ,LogMethod method = LogMethod.ToFileAndConsole)
+        public override void Log(string Message, LogLevel Flag ,LogMethod method = LogMethod.TO_FILE_AND_CONSOLE)
         {
-            string Level = "";
+            string prefix = "";
             switch(Flag)  // get the actual Flag that we will use in our log message. We'll also get the color for the message displayed in console logs
             {
-                case LogLevel.Fatal:
-                    Level = "Fatal";
+                case LogLevel.FATAL:
+                    prefix = "Fatal";
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     break;
-                case LogLevel.Error:
-                    Level = "Error";
+                case LogLevel.ERROR:
+                    prefix = "Error";
                     Console.ForegroundColor = ConsoleColor.Red;
                     break;
-                case LogLevel.Warn:
-                    Level = "Warn";
+                case LogLevel.WARN:
+                    prefix = "Warn";
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     break;
-                case LogLevel.Debug:
-                    Level = "Debug";
+                case LogLevel.DEBUG:
+                    prefix = "Debug";
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     break;
-                case LogLevel.Info:
-                    Level = "Info";
+                case LogLevel.INFO:
+                    prefix = "Info";
                     break;
             }
 
             // checks for the Logging method that you chose
-            if (method == LogMethod.ToConsole)
+            if (method == LogMethod.TO_CONSOLE)
             {
-                Console.WriteLine($"[{Level}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
+                Console.WriteLine($"[{prefix}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
             }
 
-            if (method == LogMethod.ToFile)
+            if (method == LogMethod.TO_FILE)
             {
-                using (System.IO.StreamWriter text = System.IO.File.AppendText(this.LogPath))
+                using (System.IO.StreamWriter text = System.IO.File.AppendText(this.LogFilePath))
                 {
-                    text.WriteLine($"[{Level}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
+                    text.WriteLine($"[{prefix}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
                 }
             }
             else
             {
-                using (System.IO.StreamWriter text = System.IO.File.AppendText(this.LogPath))
+                using (System.IO.StreamWriter text = System.IO.File.AppendText(this.LogFilePath))
                 {
-                    text.WriteLine($"[{Level}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
+                    text.WriteLine($"[{prefix}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
                 }
-                Console.WriteLine($"[{Level}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
+                Console.WriteLine($"[{prefix}]", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), " : ", Message);
             }
         }
 
